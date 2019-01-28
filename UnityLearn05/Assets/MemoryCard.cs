9686dbe14543b241653d2e256692d3b363f426b8
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2019-01-28 12:40:31 +0300

    Added MemoryCards project

---
+++ UnityLearn05/Assets/MemoryCard.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class MemoryCard : MonoBehaviour
+{
+    [SerializeField] private GameObject _cardBack;
+    [SerializeField] private Sprite _image;
+
+    void Start()
+    {
+        GetComponent<SpriteRenderer>().sprite = _image;
+    }
+
+    public void OnMouseDown()
+    {
+        if (_cardBack.activeSelf)
+        {
+            _cardBack.SetActive(false);
+        }
+    }
+}
