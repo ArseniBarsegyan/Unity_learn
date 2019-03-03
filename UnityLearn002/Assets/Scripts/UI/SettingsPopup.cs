77b143eac98b2cbde0a923e00f732c2866cc8612
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2019-03-03 12:03:42 +0300

    Saving speed value to PlayerPrefs

--- UnityLearn002/Assets/Scripts/UI/SettingsPopup.cs
+++ UnityLearn002/Assets/Scripts/UI/SettingsPopup.cs
@@ -1,7 +1,15 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingsPopup : MonoBehaviour
 {
+    [SerializeField] private Slider speedSlider;
+
+    void Start()
+    {
+        speedSlider.value = PlayerPrefs.GetFloat("speed", 1);
+    }
+
     public void Open()
     {
         gameObject.SetActive(true);
@@ -20,5 +28,6 @@ public class SettingsPopup : MonoBehaviour
     public void OnSpeedValue(float speed)
     {
         Debug.Log("Speed: " + speed);
+        PlayerPrefs.SetFloat("speed", speed);
     }
 }
