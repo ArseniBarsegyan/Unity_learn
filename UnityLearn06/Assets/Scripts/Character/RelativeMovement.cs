c01f4c0fd93c3d8ead44f37e0ac072c479533371
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2019-03-03 16:35:41 +0300

    Character horizontal movements

--- UnityLearn06/Assets/Scripts/Character/RelativeMovement.cs
+++ UnityLearn06/Assets/Scripts/Character/RelativeMovement.cs
@@ -1,9 +1,18 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class RelativeMovement : MonoBehaviour
 {
     [SerializeField] private Transform target;
     public float rotSpeed = 15.0f;
+    public float moveSpeed = 6.0f;
+
+    private CharacterController _charController;
+
+    void Start()
+    {
+        _charController = GetComponent<CharacterController>();
+    }
 
     void Update()
     {
@@ -12,16 +21,20 @@ public class RelativeMovement : MonoBehaviour
         float vertInput = Input.GetAxis("Vertical");
         if (horInput != 0 || vertInput != 0)
         {
-            movement.x = horInput;
-            movement.z = vertInput;
+            movement.x = horInput * moveSpeed;
+            movement.z = vertInput * moveSpeed;
+            movement = Vector3.ClampMagnitude(movement, moveSpeed);
             Quaternion tmp = target.rotation;
             target.eulerAngles = new Vector3(0, target.eulerAngles.y, 0);
-            movement = target.TransformDirection(movement);
+            // movement = target.TransformDirection(movement);
             target.rotation = tmp;
 
             Quaternion direction = Quaternion.LookRotation(movement);
             transform.rotation = Quaternion.Lerp(transform.rotation,
                 direction, rotSpeed * Time.deltaTime);
+
+            movement *= Time.deltaTime;
+            _charController.Move(movement);
         }
     }
 }
