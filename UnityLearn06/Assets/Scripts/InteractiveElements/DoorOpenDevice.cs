fdec85c57fd8d721fe5a9551a9838c4e72ad4fdd
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2019-03-21 20:00:00 +0300

    Added door and DoorOpenDevice script

---
+++ UnityLearn06/Assets/Scripts/InteractiveElements/DoorOpenDevice.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class DoorOpenDevice : MonoBehaviour
+{
+    [SerializeField] private Vector3 doorPosition;
+
+    private bool _isDoorOpen;
+
+    public void Operate()
+    {
+        if (_isDoorOpen)
+        {
+            Vector3 position = transform.position - doorPosition;
+            transform.position = position;
+        }
+        else
+        {
+            Vector3 position = transform.position + doorPosition;
+            transform.position = position;
+        }
+        _isDoorOpen = !_isDoorOpen;
+    }
+}
