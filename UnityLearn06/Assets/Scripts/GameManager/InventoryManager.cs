5719557443e5b3bd14542213d5fa7199d9ae0d1c
arseni barsegyan <arseni.barsegyan@gmail.com>
2019-04-11 19:35:15 +0300

    InventoryManager: items names stored in list

--- UnityLearn06/Assets/Scripts/GameManager/InventoryManager.cs
+++ UnityLearn06/Assets/Scripts/GameManager/InventoryManager.cs
@@ -1,12 +1,35 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
 
 public class InventoryManager : MonoBehaviour, IGameManager
 {
+    private List<string> _items;
+
     public ManagerStatus Status { get; private set; }
 
     public void Startup()
     {
         Debug.Log("Inventory manager starting...");
+        _items = new List<string>();
         Status = ManagerStatus.Started;
+    }
+
+    private void DisplayItems()
+    {
+        StringBuilder builder = new StringBuilder();
+        builder.Append("Items: ");
+        foreach (string item in _items)
+        {
+            builder.Append(item);
+            builder.Append(" ");
+        }
+        Debug.Log(builder.ToString());
+    }
+
+    public void AddItem(string name)
+    {
+        _items.Add(name);
+        DisplayItems();
     }
 }
