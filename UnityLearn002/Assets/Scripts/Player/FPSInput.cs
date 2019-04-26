7d4708d7e82e354fb3574421f68953a283b95d48
arseni barsegyan <arseni.barsegyan@gmail.com>
2019-04-26 23:58:17 +0300

    Removed unused assets. Added background music. Enemy prefab bug fixed.

---
+++ UnityLearn002/Assets/Scripts/Player/FPSInput.cs
@@ -0,0 +1,46 @@
+﻿using Assets.Scripts.Broadcast;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+[AddComponentMenu("Control Script/FPS Input")]
+public class FPSInput : MonoBehaviour
+{
+    public float speed = 6.0f;
+    public float gravity = -9.8f;
+    public const float baseSpeed = 6.0f;
+    private CharacterController _charController;
+
+    void Awake()
+    {
+        Messenger<float>.AddListener(GameEvent.SpeedChanged, OnSpeedChanged);
+    }
+
+    void OnDestroy()
+    {
+        Messenger<float>.RemoveListener(GameEvent.SpeedChanged, OnSpeedChanged);
+    }
+
+    private void OnSpeedChanged(float value)
+    {
+        speed = baseSpeed * value;
+    }
+
+    void Start()
+    {
+        _charController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        float deltaZ = Input.GetAxis("Vertical") * speed;
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        movement = Vector3.ClampMagnitude(movement, speed);
+
+        movement.y = gravity;
+
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+        _charController.Move(movement);
+    }
+}
--- UnityLearn002/Assets/Scripts/SceneController.cs
+++ UnityLearn002/Assets/Scripts/SceneController.cs
@@ -12,7 +12,14 @@ public class SceneController : MonoBehaviour
         {
             _enemy = Instantiate(enemyPrefab) as GameObject;
             var aiComponent = _enemy.GetComponent<WanderingAI>();
-            aiComponent.speed = aiComponent.baseSpeed * PlayerPrefs.GetFloat("speed");
+            if (PlayerPrefs.GetFloat("speed") == 0)
+            {
+                aiComponent.speed = aiComponent.baseSpeed;
+            }
+            else
+            {
+                aiComponent.speed = aiComponent.baseSpeed * PlayerPrefs.GetFloat("speed");
+            }
             _enemy.transform.position = new Vector3(0, 1, 0);
             float angle = Random.Range(0, 360);
             _enemy.transform.Rotate(0, angle, 0);
