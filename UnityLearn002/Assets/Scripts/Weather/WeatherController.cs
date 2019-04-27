2633eafd1031caafcbfa84a392681b8372e339f4
arseni barsegyan <arseni.barsegyan@gmail.com>
2019-04-27 13:49:23 +0300

    Added WeatherController

---
+++ UnityLearn002/Assets/Scripts/Weather/WeatherController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class WeatherController : MonoBehaviour
+{
+    [SerializeField] private Material sky;
+    [SerializeField] private Light sun;
+
+    private float _fullIntensity;
+    private float _cloudValue = 0f;
+
+    void Start()
+    {
+        _fullIntensity = sun.intensity;
+    }
+
+    void Update()
+    {
+        SetOvercast(_cloudValue);
+        if (_cloudValue <= 1.0f)
+        {
+            _cloudValue += .001f;
+        }
+    }
+
+    private void SetOvercast(float value)
+    {
+        sky.SetFloat("_Blend", value);
+        sun.intensity = _fullIntensity - (_fullIntensity * value);
+    }
+}
