e5d05041fb5546f25d8e4921e8c2a85f9af3633c
arseni barsegyan <arseni.barsegyan@gmail.com>
2019-05-03 21:23:11 +0300

    002: NetworkService updated.

--- UnityLearn002/Assets/Scripts/Managers/NetworkService.cs
+++ UnityLearn002/Assets/Scripts/Managers/NetworkService.cs
@@ -1,18 +1,44 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class NetworkService : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private const string XmlApi = "https://samples.openweathermap.org/data/2.5/forecast?id=524901&appid=b6907d289e10d714a6e88b30761fae22";
+
+    private bool IsResponseValid(UnityWebRequest webRequest)
     {
-        
+        if (webRequest.error != null)
+        {
+            Debug.Log("bad connection");
+            return false;
+        }
+
+        if (string.IsNullOrEmpty(webRequest.downloadHandler.text))
+        {
+            Debug.Log("bad data");
+            return false;
+        }
+        return true;
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator CallAPI(string url, Action<string> callback)
     {
-        
+        using (var webRequest = UnityWebRequest.Get(url))
+        {
+            yield return webRequest.SendWebRequest();
+
+            if (!IsResponseValid(webRequest))
+            {
+                yield break;
+            }
+            callback(webRequest.downloadHandler.text);
+        }
+    }
+
+    public IEnumerator GetWeatherXML(Action<string> callback)
+    {
+        return CallAPI(XmlApi, callback);
     }
 }
