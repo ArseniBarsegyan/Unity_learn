2e5332743af3d1efe4385b95e2e51ea9458cecaa
arseni barsegyan <arseni.barsegyan@gmail.com>
2019-05-27 21:28:00 +0300

    Initial commit. Added Network service, managers, inventory and player managers

---
+++ Unity_final/Assets/Scripts/Managers/NetworkService.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkService : MonoBehaviour
+{
+    private const string XmlApi = "http://api.openweathermap.org/data/2.5/weather?q=Chicago,us&APPID=e41900cf4f3ced633b4a5e9257cfb312";
+    private const string WebImageUrl = "https://upload.wikimedia.org/wikipedia/commons/c/c5/Moraine_Lake_17092005.jpg";
+    private const string LocalApiUrl = "http://localhost:8080/api/weather";
+
+    public IEnumerator DownloadImage(Action<Texture2D> callback)
+    {
+        UnityWebRequest webRequest = UnityWebRequestTexture.GetTexture(WebImageUrl);
+        yield return webRequest.SendWebRequest();
+        callback(DownloadHandlerTexture.GetContent(webRequest));
+    }
+
+    public IEnumerator GetWeather(Action<string> callback)
+    {
+        return CallAPI(XmlApi, callback);
+    }
+
+    private bool IsResponseValid(UnityWebRequest webRequest)
+    {
+        if (webRequest.error != null)
+        {
+            Debug.Log("bad connection");
+            return false;
+        }
+
+        if (string.IsNullOrEmpty(webRequest.downloadHandler.text))
+        {
+            Debug.Log("bad data");
+            return false;
+        }
+        return true;
+    }
+
+    private IEnumerator CallAPI(string url, Action<string> callback)
+    {
+        using (var webRequest = UnityWebRequest.Get(url))
+        {
+            yield return webRequest.SendWebRequest();
+
+            if (!IsResponseValid(webRequest))
+            {
+                yield break;
+            }
+            callback(webRequest.downloadHandler.text);
+        }
+    }
+
+    private IEnumerator Post(string url, Hashtable args, Action<string> callback)
+    {
+        UnityWebRequest webRequest;
+        if (args == null)
+        {
+            webRequest = new UnityWebRequest(url);
+        }
+        else
+        {
+            var form = new WWWForm();
+            foreach (DictionaryEntry arg in args)
+            {
+                form.AddField(arg.Key.ToString(), arg.Value.ToString());
+            }
+            webRequest = UnityWebRequest.Post(url, form);
+        }
+        yield return webRequest;
+    }
+
+    public IEnumerator LogWeather(string name, float cloudValue, Action<string> callback)
+    {
+        Hashtable args = new Hashtable();
+        args.Add("message", name);
+        args.Add("cloud_value", cloudValue);
+        args.Add("timestamp", DateTime.UtcNow.Ticks);
+
+        return Post(LocalApiUrl, args, callback);
+    }
+}
---
+++ Unity_final/Assets/Scripts/Managers/PlayerManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class PlayerManager : MonoBehaviour, IGameManager
+{
+    private NetworkService _network;
+
+    public ManagerStatus Status { get; private set; }
+    public int Health { get; private set; }
+    public int MaxHealth { get; set; }
+
+    public void Startup(NetworkService service)
+    {
+        Debug.Log("Player manager starting...");
+        _network = service;
+
+        Health = 50;
+        MaxHealth = 100;
+
+        Status = ManagerStatus.Started;
+    }
+
+    public void ChangeHealth(int value)
+    {
+        Health += value;
+        if (Health > MaxHealth)
+        {
+            Health = MaxHealth;
+        }
+        else if (Health < 0)
+        {
+            Health = 0;
+        }
+
+        Debug.Log("Health: " + Health + "/" + MaxHealth);
+    }
+}
