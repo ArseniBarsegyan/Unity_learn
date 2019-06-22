4e081d54c64c9e53f091e4dfd6cd07e833c52269
arseni barsegyan <arseni.barsegyan@gmail.com>
2019-06-22 15:25:14 +0300

    Finished

---
+++ Unity_final/Assets/Editor/TestPostBuild.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+public static class TestPostBuild
+{
+    [PostProcessBuild]
+    public static void OnPostprocessBuild(BuildTarget target, string
+        pathToBuiltProject)
+    {
+        Debug.Log("build location: " + pathToBuiltProject);
+    }
+}
