002372ece56027e042bd647a105cc17b31583648
Arseni Barsegyan <Arseni Barsegyan@gmail.com>
2019-06-26 13:08:00 +0300

    Added PriorityQueue class and Add method (binary heap)

---
+++ Pathfinding/Assets/Scripts/PriorityQueue.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class PriorityQueue<T> where T : IComparable<T>
+{
+    private List<T> data;
+
+    public PriorityQueue()
+    {
+        data = new List<T>();
+    }
+
+    public void Enqueue(T item)
+    {
+        data.Add(item);
+
+        int childIndex = data.Count - 1;
+
+        while (childIndex > 0)
+        {
+            int parentIndex = (childIndex - 1) / 2;
+            if (data[childIndex].CompareTo(data[parentIndex]) >= 0)
+            {
+                break;
+            }
+
+            T tmp = data[childIndex];
+            data[childIndex] = tmp;
+            data[parentIndex] = tmp;
+
+            childIndex = parentIndex;
+        }
+    }
+
+}
