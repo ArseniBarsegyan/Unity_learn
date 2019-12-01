a6130c4cdc2d5d9419589bc3b50f9564ba86e847
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2019-12-01 20:27:33 +0300

    Added PlayerManager, PlayerInput scripts. Moving player depend on user input

---
+++ GoBoard/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    private float m_h;
+    private float m_v;
+
+    private bool m_inputEnabled = false;
+
+    public float H { get => m_h; }
+    public float V { get => m_v; }
+
+    public bool InputEnabled
+    {
+        get => m_inputEnabled;
+        set => m_inputEnabled = value;
+    }
+
+    public void GetKeyInput()
+    {
+        if (InputEnabled)
+        {
+            m_h = Input.GetAxisRaw("Horizontal");
+            m_v = Input.GetAxisRaw("Vertical");
+        }
+    }
+}
--- GoBoard/Assets/Scripts/PlayerMover.cs
+++ GoBoard/Assets/Scripts/PlayerMover.cs
@@ -12,11 +12,19 @@ public class PlayerMover : MonoBehaviour
 
     void Start()
     {
-        Move(new Vector3(2f,0f,0f), 1f);
-        Move(new Vector3(4f, 0f, 0f), 3f);
-        Move(new Vector3(4f, 0f, 2f), 5f);
-        Move(new Vector3(4f, 0f, 4f), 7f);
     }
+
+    //IEnumerator Test()
+    //{
+    //    yield return new WaitForSeconds(1f);
+    //    MoveRight();
+    //    yield return new WaitForSeconds(2f);
+    //    MoveRight();
+    //    yield return new WaitForSeconds(2f);
+    //    MoveForward();
+    //    yield return new WaitForSeconds(2f);
+    //    MoveForward();
+    //}
 
     public void Move(Vector3 destinationPos, float delayTime = 0.25f)
     {
@@ -46,5 +54,29 @@ public class PlayerMover : MonoBehaviour
         iTween.Stop(gameObject);
         transform.position = destinationPos;
         isMoving = false;
+    }
+
+    public void MoveLeft()
+    {
+        Vector3 newPosition = transform.position + new Vector3(-2, 0, 0);
+        Move(newPosition, 0);
+    }
+
+    public void MoveRight()
+    {
+        Vector3 newPosition = transform.position + new Vector3(2, 0, 0);
+        Move(newPosition, 0);
+    }
+
+    public void MoveForward()
+    {
+        Vector3 newPosition = transform.position + new Vector3(0, 0, 2);
+        Move(newPosition, 0);
+    }
+
+    public void MoveBackward()
+    {
+        Vector3 newPosition = transform.position + new Vector3(0, 0, -2);
+        Move(newPosition, 0);
     }
 }
