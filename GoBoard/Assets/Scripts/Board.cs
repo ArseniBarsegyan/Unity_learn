8c1175a1c5ef0df9912935d6c8afa3a122bd2d64
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2019-12-05 21:29:23 +0300

    Added Utility (rounding vectors helper), finding neighbors algorithm (Node class), List of all Nodes in Board script.

--- GoBoard/Assets/Scripts/Board.cs
+++ GoBoard/Assets/Scripts/Board.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class Board : MonoBehaviour
 {
@@ -10,4 +11,21 @@ public class Board : MonoBehaviour
         new Vector2(0f, spacing),
         new Vector2(0f, -spacing)
     };
+
+    List<Node> m_allNodes = new List<Node>();
+    public List<Node> AllNodes
+    {
+        get => m_allNodes;
+    }
+    
+    void Awake()
+    {
+        GetNodeList();
+    }
+
+    public void GetNodeList()
+    {
+        Node[] nList = GameObject.FindObjectsOfType<Node>();
+        m_allNodes = new List<Node>(nList);
+    }
 }
--- GoBoard/Assets/Scripts/Node.cs
+++ GoBoard/Assets/Scripts/Node.cs
@@ -4,6 +4,26 @@ using UnityEngine;
 
 public class Node : MonoBehaviour
 {
+    Vector2 m_coordinate;
+    public Vector2 Coordinate
+    {
+        get => Utility.Vector2Round(m_coordinate);
+    }
+
+    List<Node> m_neighborNodes = new List<Node>();
+    public List<Node> NeigborNodes
+    {
+        get => m_neighborNodes;
+    }
+
+    Board m_board;
+
+    private void Awake()
+    {
+        m_board = FindObjectOfType<Board>();
+        m_coordinate = new Vector2(transform.position.x, transform.position.z);
+    }
+
     [SerializeField] private GameObject geometry;
     public float scaleTime = 0.3f;
     public iTween.EaseType easeType = iTween.EaseType.easeInExpo;
@@ -21,6 +41,10 @@ public class Node : MonoBehaviour
         {
             ShowGeometry();
         }
+        if (m_board != null)
+        {
+            m_neighborNodes = FindNeighbors(m_board.AllNodes);
+        }
     }
 
     public void ShowGeometry()
@@ -34,5 +58,21 @@ public class Node : MonoBehaviour
                 "delay", delay
                 ));
         }
+    }
+
+    public List<Node> FindNeighbors(List<Node> nodes)
+    {
+        List<Node> nList = new List<Node>();
+
+        foreach(var dir in Board.directions)
+        {
+            var foundNeighbor = nodes.Find(x => x.Coordinate == Coordinate + dir);
+
+            if (foundNeighbor != null && !nList.Contains(foundNeighbor))
+            {
+                nList.Add(foundNeighbor);
+            }
+        }
+        return nList;
     }
 }
