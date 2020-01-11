4e3f46ca56c46bb0fd91057c2123cb4a6893bd39
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2020-01-11 22:55:25 +0300

    Added PostProcessingBlur effect to main camera after level is completed

---
+++ GoBoard/Assets/Scripts/EndScreen.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class EndScreen : MonoBehaviour
+{
+    public PostProcessProfile blurProfile;
+    public PostProcessProfile normalProfile;
+    public PostProcessVolume postProcessVolume;
+
+    public void EnableCameraBlur(bool state)
+    {
+        if (blurProfile != null && normalProfile != null && postProcessVolume != null)
+        {
+            postProcessVolume.profile = (state) ? blurProfile : normalProfile;
+        }
+    }
+}
